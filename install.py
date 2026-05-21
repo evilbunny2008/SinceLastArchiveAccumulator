ddf327892f7b2350ff2d4f9f3e8979eb99482ea1
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-05-21 14:45:37 +1000

    Create install.py

---
+++ install.py
@@ -0,0 +1,29 @@
+""" Installer for SinceLastArchiveAccumulator service. """
+
+import configobj
+
+from weecfg.extension import ExtensionInstaller
+
+VERSION = "0.0.1"
+
+def loader():
+    """ Load and return the extension installer. """
+    return SinceLastArchiveAccumulatorInstaller()
+
+class SinceLastArchiveAccumulatorInstaller(ExtensionInstaller):
+    """ The extension installer. """
+    def __init__(self):
+
+        install_dict = {
+            'version': VERSION,
+            'name': 'SinceLastArchiveAccumulator',
+            # add a leading space, so that long versions does not run into the description
+            'description': ' A simple accumulator to provide the totals since the last archive cycle',
+            'author': "John Smith",
+            'author_email': "deltafoxtrot256+SinceLastArchiveAccumulator@gmail.com",
+            'files': [('bin/user', ['bin/user/SinceLastArchiveAccumulator.py'])]
+        }
+
+        install_dict['prep_services'] = 'user.SinceLastArchiveAccumulator.Service'
+
+        super().__init__(install_dict)
