e84faab9efd9183ac39639565c0a27e43c2fb0e4
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-05-21 21:27:02 +1000

    Everything's now working

--- bin/user/SinceLastArchiveAccumulator.py
+++ bin/user/SinceLastArchiveAccumulator.py
@@ -6,6 +6,9 @@ log = logging.getLogger(__name__)
 
 log.info("SinceLastArchiveAccumulatorService running...")
 
+weewx.units.obs_group_dict["rain_since_last_archive"] = "group_rain"
+weewx.units.obs_group_dict["ET_since_last_archive"] = "group_rain"
+
 class SinceLastArchiveAccumulatorService(weewx.engine.StdService):
 
     def __init__(self, engine, config_dict):
@@ -17,13 +20,37 @@ class SinceLastArchiveAccumulatorService(weewx.engine.StdService):
 
         self.rain = 0
         self.ET = 0
+        self.values_loop = {}
+        self.values_archive = {}
 
     def handle_archive_record(self, event):
       """ Handle archive records """
+
+      self.ET = 0
+      self.rain = 0
+
+      self.values_archive.update(event.record)
+      event.record = self.values_archive
+
       log.info(f"New archive record! {event.record}")
 
     def new_loop_packet(self, event):
       """ Handle loop packets """
 
-      updated_record = weewx.units.to_std_system(event.packet, topic_dict['unit_system'])
-      log.info(f"New loop packet {updated_record}")
+      final_record = event.packet
+
+      ET = final_record.get("ET")
+      if ET is None:
+          ET = 0
+
+      rain = final_record.get("rain")
+      if rain is None:
+          rain = 0
+
+      self.ET += ET
+      self.rain += rain
+      final_record["ET_since_last_archive"] = self.ET
+      final_record["rain_since_last_archive"] = self.rain
+
+      self.values_loop.update(final_record)
+      event.packet = self.values_loop
